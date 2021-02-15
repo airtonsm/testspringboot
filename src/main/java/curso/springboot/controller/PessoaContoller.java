7e491ab06efa77530f9699230684bab0a86f2ddf
José Airton <airtonsmorais28@gmail.com>
2021-02-15 15:30:31 -0300

    added method search people for name

--- src/main/java/curso/springboot/controller/PessoaContoller.java
+++ src/main/java/curso/springboot/controller/PessoaContoller.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
@@ -15,29 +17,30 @@ import curso.springboot.repository.PessoaRepository;
 
 @Controller
 public class PessoaContoller {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
-	@RequestMapping(method=RequestMethod.GET, value="/cadastropessoa")
+
+	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public ModelAndView inicio() {
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		modelAndView.addObject("pessoaobj", new Pessoa());
-		
+
 		return modelAndView;
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa") //dois asteríscos ignora o que vem antes na aurl
+
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa") // dois asteríscos ignora o que vem antes na
+																			// aurl
 	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-		
+
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoasIt);
 		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
 	public ModelAndView pessoas() {
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
@@ -46,30 +49,37 @@ public class PessoaContoller {
 		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
-	
-	
+
 	@GetMapping("/editarpessoa/{idpessoa}")
 	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
-		
+
 		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
-		
+
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		modelAndView.addObject("pessoaobj", pessoa.get());
 		return modelAndView;
-		
+
 	}
-	
+
 	@GetMapping("/removerpessoa/{idpessoa}")
 	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
-		
+
 		pessoaRepository.deleteById(idpessoa);
-		
+
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		modelAndView.addObject("pessoas", pessoaRepository.findAll());
 		modelAndView.addObject("pessoaobj", new Pessoa());
 		return modelAndView;
-		
 	}
 	
+	@PostMapping("**/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa ) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		
+		return modelAndView;		
+	}
 
 }
--- src/main/java/curso/springboot/repository/PessoaRepository.java
+++ src/main/java/curso/springboot/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package curso.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,5 +14,7 @@ import curso.springboot.model.Pessoa;
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
 	
 	
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoaByName(String nome);
 
 }
