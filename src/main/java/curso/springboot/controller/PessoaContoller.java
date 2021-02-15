c7445c8b0b018d9da7e64fb2e1e770c84bfb1236
José Airton <airtonsmorais28@gmail.com>
2021-02-15 15:53:56 -0300

    creating routes for phones page

--- src/main/java/curso/springboot/controller/PessoaContoller.java
+++ src/main/java/curso/springboot/controller/PessoaContoller.java
@@ -25,6 +25,8 @@ public class PessoaContoller {
 	public ModelAndView inicio() {
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		modelAndView.addObject("pessoaobj", new Pessoa());
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas", pessoasIt);
 
 		return modelAndView;
 	}
@@ -81,5 +83,17 @@ public class PessoaContoller {
 		
 		return modelAndView;		
 	}
+	
+	@GetMapping("/telefones/{idpessoa}")
+	public ModelAndView telefones(@PathVariable("idpessoa") Long idpessoa) {
+
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		return modelAndView;
+
+	}
+	
 
 }
