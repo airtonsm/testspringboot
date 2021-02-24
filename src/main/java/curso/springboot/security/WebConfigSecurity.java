ab3447f6a2c55ae40cca72240dd00d4f97b8720b
José Airton <airtonsmorais28@gmail.com>
2021-02-23 21:07:22 -0300

    validation per DB

--- src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package curso.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,6 +14,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 	
 	@Override //configura as solicitações de acesso por http
 	protected void configure(HttpSecurity http) throws Exception {
@@ -28,10 +32,18 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
 	
 	@Override // cria autenticação do usuário com o banco ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// Criptografia em banco
+		auth.userDetailsService(implementacaoUserDetailsService) 
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+		
+		/* 			------ AUTENTICAÇÃO EM MEMÓRIA-------------
 		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
 		.withUser("airton")
-		.password("$2a$10$IMRdE.TTV5gMkiOnZDQKl.VdxOfoZy6F7eW.demLk3jZsBWJWFfe6")
-		.roles("ADMIN");
+		.password("$2a$10$IMRdE.TTV5gMkiOnZDQKl.VdxOfoZy6F7eW.demLk3jZsBWJWFfe6") // SENHA CRIPTOGRADA
+		.roles("ADMIN"); 
+		*/
 	}
 	
 	@Override // ignora URL especificas (
