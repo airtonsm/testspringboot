429e8ba16bce5091634914e3e18ae9d778c10206
José Airton <airtonsmorais28@gmail.com>
2021-02-28 12:02:27 -0300

    condition for search to people

--- src/main/java/curso/springboot/controller/PessoaContoller.java
+++ src/main/java/curso/springboot/controller/PessoaContoller.java
@@ -124,10 +124,18 @@ public class PessoaContoller {
 	
 	@PostMapping("**/pesquisarpessoa")
 	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa, 
-			@RequestParam("pesqsexo") String pesqsexo ) {		
+			@RequestParam("pesqsexo") String pesqsexo ) {
+		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
+		if(pesqsexo != null && !pesqsexo.isEmpty()) {
+			pessoas = pessoaRepository.findPessoaByNameSexo(nomepesquisa, pesqsexo);
+		}else {
+			pessoas =  pessoaRepository.findPessoaByName(nomepesquisa);
+		}
 		
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
-		modelAndView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
+		modelAndView.addObject("pessoas", pessoas);
 		modelAndView.addObject("pessoaobj", new Pessoa());
 		
 		return modelAndView;		
