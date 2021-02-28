6768f5657424ba5c771cb7b468babaa75b93f3f1
José Airton <airtonsmorais28@gmail.com>
2021-02-28 15:04:07 -0300

    method GET for generate reports created

--- src/main/java/curso/springboot/controller/PessoaContoller.java
+++ src/main/java/curso/springboot/controller/PessoaContoller.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +34,9 @@ public class PessoaContoller {
 	
 	@Autowired
 	private TelefoneRepository telefoneRepository;
+	
+	@Autowired
+	private ReportUtil reportUtil;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public ModelAndView inicio() {
@@ -141,7 +146,13 @@ public class PessoaContoller {
 		return modelAndView;		
 	}
 	
-
+	@GetMapping("**/pesquisarpessoa")
+	public void imprimePdf(@RequestParam("nomepesquisa") String nomepesquisa, 
+			@RequestParam("pesqsexo") String pesqsexo, 
+			HttpServletRequest request,
+			HttpServletRequest reponse) {
+		System.out.println("impremePdf");
+	}
 	
 	@PostMapping("**/addfonePessoa/{pessoaid}")
 	public ModelAndView addFonePessoa(Telefone telefone, 
